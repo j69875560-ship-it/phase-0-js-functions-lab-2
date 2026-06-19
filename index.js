9b0d9c04bf19400069c3239d6d4ef30758b0c418
Johncarlos <j69875560@gmail.com>
2026-06-19 12:44:35 +0300

    2nd Test.

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 function calculateTax( amount) {
-  return amount * 0.01;
+  return amount *0.10;
 }
 function convertToUpperCase( text) {
   return text.toUpperCase();
 }
-function findMaximum(num1, num2) {
+function findMaximum( num1, num2) {
   if (num1 > num2) {
     return num1;
   } else {
     return num2;
   }
 }
-function isPalindrome( word) { String reversedWord = new StringBuilder(word).reverse().toString();
+function isPalindrome( word) { 
+    let reversedWord = word.split('').reverse().join('');
     if (word === reversedWord) {
         return true;
     } else {
@@ -21,7 +22,7 @@ function isPalindrome( word) { String reversedWord = new StringBuilder(word).rev
         
 
 
-function calculateDiscountedPrice(originalPrice, discountPercentage) {
+function calculateDiscountedPrice( originalPrice, discountPercentage) {
   let discountedPrice = originalPrice * (discountPercentage / 100);
   return originalPrice - discountedPrice;
 }
