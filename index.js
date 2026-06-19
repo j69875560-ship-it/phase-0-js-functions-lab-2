0f8df65df1adbc2ac9e5ca621e79e0441724fcfa
Johncarlos <j69875560@gmail.com>
2026-06-19 12:12:26 +0300

    Adding the findMaximum function.

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ function calculateTax(amount) {
 function convertToUpperCase(str) {
   return str.toUpperCase();
 }
+function findMaximum(num1, num2) {
+  if (num1 > num2) {
+    return num1;
+  } else {
+    return num2;
+  }
+}
