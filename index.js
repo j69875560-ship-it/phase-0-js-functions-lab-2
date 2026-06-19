90f58df78c5973e268b152f80a9c6e7e830c6a21
Johncarlos <j69875560@gmail.com>
2026-06-19 12:10:30 +0300

    Adding the calculate tax function.

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-
-
-
-
-
-// This is required for the test to function properly  
-module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
+function calculateTax(amount){
+    return amount * 0.01;
+}
