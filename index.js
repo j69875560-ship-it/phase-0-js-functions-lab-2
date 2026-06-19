40a3f852efa1d0993a002999f817d9c444d0d814
Johncarlos <j69875560@gmail.com>
2026-06-19 12:13:24 +0300

    Adding the isPalindrome function.

--- index.js
+++ index.js
@@ -11,3 +11,8 @@ function findMaximum(num1, num2) {
     return num2;
   }
 }
+function isPalindrome(str) {
+  const cleanedStr = str.replace(/[^A-Za-z0-9]/g, "").toLowerCase();
+  return cleanedStr === cleanedStr.split("").reverse().join("");
+}
+
