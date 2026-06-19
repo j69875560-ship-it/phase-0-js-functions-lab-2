1113a017c9706c7e35601073851967bf5676387f
Johncarlos <j69875560@gmail.com>
2026-06-19 12:14:27 +0300

    Adding the calculatingDiscountPrice Function.

--- index.js
+++ index.js
@@ -16,3 +16,15 @@ function isPalindrome(str) {
   return cleanedStr === cleanedStr.split("").reverse().join("");
 }
 
+function calculateDiscountedPrice(price, discount) {
+  return price - price * (discount / 100);
+}
+
+// This is required for the test to function properly
+module.exports = {
+  calculateTax,
+  convertToUpperCase,
+  findMaximum,
+  isPalindrome,
+  calculateDiscountedPrice,
+};
